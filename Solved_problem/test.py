d3b62a8a890ce53f301eb53ca94e9abf40fb2b84
robertDouwnyJ <dkdltl121255@gmail.com>
2026-03-21 00:20:43 +0900

    26-03-21

--- Solved_problem/test.py
+++ Solved_problem/test.py
@@ -1,16 +1,4 @@
-r=0
-s = input("Type of Mineral = ").strip()
-num = int(input("Number of Mineral = "))
-match s:
-    case "diamond":
-        r=150*4*num
-    case "ruby":
-        r=30*4*num
-    case "coral":
-        r=10*4*num
-    case _:
-        r = 0
+n = int(input())
+dp = [0]* (n+1)
 
-
-print("savings =", r)
-
+print(dp)
